89c6c6858df39ac9a660d859631e5b17c77ed119
Quentin Neves <neves.quentin@gmail.com>
2022-03-11 09:18:17 +0100

    Set up basic resource text

--- Assets/ResourceUI.cs
+++ Assets/ResourceUI.cs
@@ -5,9 +5,16 @@ using TMPro;
 
 public class ResourceUI : MonoBehaviour
 {
+    public NNSG.Goods.Good good { get; set; }
+    
     [SerializeField]
     private TextMeshProUGUI _label;
 
     [SerializeField]
     private TextMeshProUGUI _value;
+
+    public void UpdateValue(float value)
+    {
+        _value.SetText(value.ToString());
+    }
 }
