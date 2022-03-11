7159bbbd422c4cd35d382b03a6c4efffa6da25aa
Quentin Neves <neves.quentin@gmail.com>
2022-03-11 09:12:29 +0100

    Create ResourceUI component
    This will be instantiated to display Good values

---
+++ Assets/ResourceUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _label;
+
+    [SerializeField]
+    private TextMeshProUGUI _value;
+}
