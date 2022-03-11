756171d64a8923b4e86d6242285291222c5fc955
Quentin Neves <neves.quentin@gmail.com>
2022-03-11 10:40:04 +0100

    Create ResourceUI prefab

---
+++ Assets/_Scripts/ResourceDisplayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NNSG;
+using NNSG.Goods;
+
+public class ResourceDisplayer : MonoBehaviour
+{
+    List<Good> goods = new List<Good>();
+    [SerializeField]
+    private ResourceUI _resourceUI;
+
+    private void Start() {
+        // Get all goods
+        goods.Add(Warehouse.food);
+        goods.Add(Warehouse.furniture);
+        goods.Add(Warehouse.clothes);
+        goods.Add(Warehouse.vehicles);
+
+        // Check if Warehouse was correctly initialized
+        if (goods.Count > 0 && goods[0] != null)
+        {
+
+        }
+    }
+}
